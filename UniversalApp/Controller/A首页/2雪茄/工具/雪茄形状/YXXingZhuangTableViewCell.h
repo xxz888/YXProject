856c79220c29e5fb288c6a2b3a90e91867050a5b
xxz888 <6952731@qq.com>
2019-02-14 18:14:09 +0800

    no message

---
+++ UniversalApp/Controller/A首页/2雪茄/工具/雪茄形状/YXXingZhuangTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  YXXingZhuangTableViewCell.h
+//  UniversalApp
+//
+//  Created by 小小醉 on 2019/2/14.
+//  Copyright © 2019年 徐阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YXXingZhuangTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ UniversalApp/Controller/A首页/2雪茄/工具/雪茄形状/YXXingZhuangTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  YXXingZhuangTableViewCell.m
+//  UniversalApp
+//
+//  Created by 小小醉 on 2019/2/14.
+//  Copyright © 2019年 徐阳. All rights reserved.
+//
+
+#import "YXXingZhuangTableViewCell.h"
+
+@implementation YXXingZhuangTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
