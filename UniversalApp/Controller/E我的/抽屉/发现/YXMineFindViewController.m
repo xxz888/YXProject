6004437fc6802ed0d736f485a71eaf7e84346422
xxz888 <6952731@qq.com>
2019-03-25 15:37:37 +0800

    no message

--- UniversalApp/Controller/E我的/抽屉/发现/YXMineFindViewController.m
+++ UniversalApp/Controller/E我的/抽屉/发现/YXMineFindViewController.m
@@ -19,10 +19,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"发现好友";
-    // Do any additional setup after loading the view from its nib.
     [self.yxTableView registerNib:[UINib nibWithNibName:@"YXMineFindTableViewCell" bundle:nil] forCellReuseIdentifier:@"YXMineFindTableViewCell"];
     self.yxTableView.separatorStyle = 0;
-    
 }
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     return 10;
@@ -32,21 +30,15 @@
 }
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     YXMineFindTableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"YXMineFindTableViewCell" forIndexPath:indexPath];
-    
-    
-    
     [cell.titleImv sd_setImageWithURL:[NSURL URLWithString:@""] placeholderImage:[UIImage imageNamed:@"img_moren"]];
-
     return cell;
 }
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)wechat1:(id)sender {
+    
 }
-*/
+- (IBAction)wechat2:(id)sender {
+}
+- (IBAction)wechat3:(id)sender {
+}
 
 @end
