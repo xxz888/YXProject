6fa0645dc971859815bba86809b682526c250d5b
xxz888 <6952731@qq.com>
2019-09-01 10:09:20 +0800

    no message

---
+++ UniversalApp/Controller/E我的/积分/YXMineJiFenTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  YXMineJiFenTableViewController.h
+//  UniversalApp
+//
+//  Created by 小小醉 on 2019/8/30.
+//  Copyright © 2019 徐阳. All rights reserved.
+//
+
+#import "RootTableViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YXMineJiFenTableViewController : RootTableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
