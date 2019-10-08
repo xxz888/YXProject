1bb7d4aebb05aba79041f90d2f0fc9b79f8c3450
xxz888 <6952731@qq.com>
2019-10-08 16:33:47 +0800

    no message

---
+++ UniversalApp/Controller/B发现/发布弹框vc/YXFaBuNewVCViewController.h
@@ -0,0 +1,17 @@
+//
+//  YXFaBuNewVCViewController.h
+//  UniversalApp
+//
+//  Created by 小小醉 on 2019/10/6.
+//  Copyright © 2019 徐阳. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YXFaBuNewVCViewController : RootViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
