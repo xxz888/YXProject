a17b3cb992e8e0488dcfd43e8e9ef389673ada96
xxz888 <6952731@qq.com>
2019-10-28 16:07:19 +0800

    no message

---
+++ UniversalApp/Manager/UserManager/SYBaseHttpConnection.h
@@ -0,0 +1,17 @@
+//
+//  SYBaseHttpConnection.h
+//  UniversalApp
+//
+//  Created by 小小醉 on 2019/10/28.
+//  Copyright © 2019 徐阳. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SYBaseHttpConnection : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ UniversalApp/Manager/UserManager/SYBaseHttpConnection.m
@@ -0,0 +1,13 @@
+//
+//  SYBaseHttpConnection.m
+//  UniversalApp
+//
+//  Created by 小小醉 on 2019/10/28.
+//  Copyright © 2019 徐阳. All rights reserved.
+//
+
+#import "SYBaseHttpConnection.h"
+
+@implementation SYBaseHttpConnection
+
+@end
