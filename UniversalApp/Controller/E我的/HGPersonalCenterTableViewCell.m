e716be23eceeae1fd96218c25c160981c60055b5
xxz888 <6952731@qq.com>
2019-11-18 16:12:19 +0800

    no message

---
+++ UniversalApp/Controller/E我的/HGPersonalCenterTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  HGPersonalCenterTableViewCell.m
+//  UniversalApp
+//
+//  Created by 小小醉 on 2019/11/18.
+//  Copyright © 2019 徐阳. All rights reserved.
+//
+
+#import "HGPersonalCenterTableViewCell.h"
+
+@implementation HGPersonalCenterTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
