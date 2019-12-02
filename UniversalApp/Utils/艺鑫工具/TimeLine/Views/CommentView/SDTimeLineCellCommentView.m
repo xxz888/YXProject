af11a98504f352256e78425fe4246f27c7a9a1dc
xxz888 <6952731@qq.com>
2019-12-02 14:30:24 +0800

    no message

---
+++ UniversalApp/Utils/艺鑫工具/TimeLine/Views/CommentView/SDTimeLineCellCommentView.m
@@ -0,0 +1,272 @@
+//
+//  SDTimeLineCellCommentView.m
+//  GSD_WeiXin(wechat)
+//
+//  Created by gsd on 16/2/25.
+//  Copyright © 2016年 GSD. All rights reserved.
+//
+
+/*
+ 
+ *********************************************************************************
+ *
+ * GSD_WeiXin
+ *
+ * QQ交流群: 459274049
+ * Email : gsdios@126.com
+ * GitHub: https://github.com/gsdios/GSD_WeiXin
+ * 新浪微博:GSD_iOS
+ *
+ * 此“高仿微信”用到了很高效方便的自动布局库SDAutoLayout（一行代码搞定自动布局）
+ * SDAutoLayout地址：https://github.com/gsdios/SDAutoLayout
+ * SDAutoLayout视频教程：http://www.letv.com/ptv/vplay/24038772.html
+ * SDAutoLayout用法示例：https://github.com/gsdios/SDAutoLayout/blob/master/README.md
+ *
+ *********************************************************************************
+ 
+ */
+
+#import "SDTimeLineCellCommentView.h"
+#import "UIView+SDAutoLayout.h"
+#import "SDTimeLineCellModel.h"
+
+#import "LEETheme.h"
+
+@interface SDTimeLineCellCommentView () <MLLinkLabelDelegate>
+
+@property (nonatomic, strong) NSArray *likeItemsArray;
+@property (nonatomic, strong) NSArray *commentItemsArray;
+
+@property (nonatomic, strong) UIImageView *bgImageView;
+
+
+@property (nonatomic, strong) UIView *likeLableBottomLine;
+
+@property (nonatomic, strong) NSMutableArray *commentLabelsArray;
+
+
+@end
+
+@implementation SDTimeLineCellCommentView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        
+        [self setupViews];
+    
+        //设置主题
+        [self configTheme];
+
+    }
+    return self;
+}
+
+- (void)setupViews
+{
+    _bgImageView = [UIImageView new];
+//        UIImage *bgImage = [UIImage imageNamed:@""];
+//    _bgImageView.image = bgImage;
+    //self.backgroundColor = YXRGBAColor(236, 236, 236);
+    //[self addSubview:_bgImageView];
+    
+    _likeLabel = [MLLinkLabel new];
+    _likeLabel.font = [UIFont systemFontOfSize:13];
+    _likeLabel.linkTextAttributes = @{NSForegroundColorAttributeName : TimeLineCellHighlightedColor};
+    _likeLabel.isAttributedContent = YES;
+//    [self addSubview:_likeLabel];
+    
+    _likeStringLabel = [MLLinkLabel new];
+    _likeStringLabel.text = @"人觉得很赞";
+    _likeStringLabel.font = [UIFont systemFontOfSize:13];
+    _likeStringLabel.linkTextAttributes = @{NSForegroundColorAttributeName : TimeLineCellHighlightedColor};
+    _likeStringLabel.isAttributedContent = YES;
+//    [self addSubview:_likeStringLabel];
+    
+    _likeLableBottomLine = [UIView new];
+    [self addSubview:_likeLableBottomLine];
+    
+    _bgImageView.sd_layout.spaceToSuperView(UIEdgeInsetsMake(0, 0, 0, 0));
+}
+
+- (void)configTheme{
+    
+
+}
+
+- (void)setCommentItemsArray:(NSArray *)commentItemsArray
+{
+    _commentItemsArray = commentItemsArray;
+    
+    long originalLabelsCount = self.commentLabelsArray.count;
+    long needsToAddCount = commentItemsArray.count > originalLabelsCount ? (commentItemsArray.count - originalLabelsCount) : 0;
+    for (int i = 0; i < needsToAddCount; i++) {
+        MLLinkLabel *label = [MLLinkLabel new];
+        label.font = [UIFont fontWithName:@"苹方-简" size:14];
+        label.delegate = self;
+        [self addSubview:label];
+        [self.commentLabelsArray addObject:label];
+    }
+    
+    for (int i = 0; i < commentItemsArray.count; i++) {
+        SDTimeLineCellCommentItemModel *model = commentItemsArray[i];
+        MLLinkLabel *label = self.commentLabelsArray[i];
+        if (!model.attributedContent) {
+            model.attributedContent = [self generateAttributedStringWithCommentItemModel:model];
+        }
+        label.attributedText = model.attributedContent;
+        label.tag = model.labelTag;
+    }
+}
+
+- (void)setLikeItemsArray:(NSArray *)likeItemsArray
+{
+    _likeItemsArray = likeItemsArray;
+    
+    NSTextAttachment *attach = [NSTextAttachment new];
+//    attach.image = [UIImage imageNamed:@"Like"];
+//    attach.bounds = CGRectMake(0, -3, 16, 16);
+ //   NSAttributedString *likeIcon = [NSAttributedString attributedStringWithAttachment:attach];
+    
+//    NSMutableAttributedString *attributedText = [[NSMutableAttributedString alloc] initWithAttributedString:likeIcon];
+    NSMutableAttributedString *attributedText = [[NSMutableAttributedString alloc]init];
+    for (int i = 0; i < likeItemsArray.count; i++) {
+        SDTimeLineCellLikeItemModel *model = likeItemsArray[i];
+        if (i > 0) {
+            [attributedText appendAttributedString:[[NSAttributedString alloc] initWithString:@"，"]];
+        }
+        if (!model.attributedContent) {
+            model.attributedContent = [self generateAttributedStringWithLikeItemModel:model];
+        }
+        [attributedText appendAttributedString:model.attributedContent];
+    }
+    
+   _likeLabel.attributedText = [attributedText copy];
+}
+
+- (NSMutableArray *)commentLabelsArray
+{
+    if (!_commentLabelsArray) {
+        _commentLabelsArray = [NSMutableArray new];
+    }
+    return _commentLabelsArray;
+}
+
+- (void)setupWithLikeItemsArray:(NSArray *)likeItemsArray commentItemsArray:(NSArray *)commentItemsArray
+{
+    self.likeItemsArray = likeItemsArray;
+    self.commentItemsArray = commentItemsArray;
+    
+    if (self.commentLabelsArray.count) {
+        [self.commentLabelsArray enumerateObjectsUsingBlock:^(UILabel *label, NSUInteger idx, BOOL *stop) {
+            [label sd_clearAutoLayoutSettings];
+            label.hidden = YES; //重用时先隐藏所以评论label，然后根据评论个数显示label
+        }];
+    }
+    
+    if (!commentItemsArray.count && !likeItemsArray.count) {
+        self.fixedWidth = @(0); // 如果没有评论或者点赞，设置commentview的固定宽度为0（设置了fixedWith的控件将不再在自动布局过程中调整宽度）
+        self.fixedHeight = @(0); // 如果没有评论或者点赞，设置commentview的固定高度为0（设置了fixedHeight的控件将不再在自动布局过程中调整高度）
+        return;
+    } else {
+        self.fixedHeight = nil; // 取消固定宽度约束
+        self.fixedWidth = nil; // 取消固定高度约束
+    }
+    
+    CGFloat margin = 5;
+    
+    UIView *lastTopView = nil;
+    
+    if (likeItemsArray.count) {
+        _likeLabel.sd_resetLayout
+        .leftSpaceToView(self, margin)
+        .rightSpaceToView(self, margin)
+        .topSpaceToView(lastTopView, 10)
+        .autoHeightRatio(0);
+        
+        lastTopView = _likeLabel;
+    } else {
+        _likeLabel.attributedText = nil;
+        _likeLabel.sd_resetLayout
+        .heightIs(0);
+    }
+    
+    
+    if (self.commentItemsArray.count && self.likeItemsArray.count) {
+        _likeLableBottomLine.sd_resetLayout
+        .leftSpaceToView(self, 0)
+        .rightSpaceToView(self, 0)
+        .heightIs(1)
+        .topSpaceToView(lastTopView, 3);
+        
+        lastTopView = _likeLableBottomLine;
+    } else {
+        _likeLableBottomLine.sd_resetLayout.heightIs(0);
+    }
+    
+    for (int i = 0; i < self.commentItemsArray.count; i++) {
+        UILabel *label = (UILabel *)self.commentLabelsArray[i];
+        label.hidden = NO;
+        CGFloat topMargin = 10 ;//(i == 0 && likeItemsArray.count == 0) ? 10 : 5;
+        label.sd_layout
+        .leftSpaceToView(self, 0)
+        .rightSpaceToView(self, 0)
+        .topSpaceToView(lastTopView, topMargin)
+        .autoHeightRatio(0);
+        
+        label.isAttributedContent = YES;
+        lastTopView = label;
+    }
+    
+    _likeStringLabel.sd_layout.leftSpaceToView(_likeLabel, 2)
+    .centerYEqualToView(_likeLabel)
+    .widthIs(100)
+    .heightIs(0);
+    [self setupAutoHeightWithBottomView:lastTopView bottomMargin:0];
+}
+
+- (void)setFrame:(CGRect)frame
+{
+    [super setFrame:frame];
+}
+
+#pragma mark - private actions
+
+- (NSMutableAttributedString *)generateAttributedStringWithCommentItemModel:(SDTimeLineCellCommentItemModel *)model{
+    NSString *text = model.firstUserName;
+    if (model.secondUserName.length) {
+        text = [text stringByAppendingString:[NSString stringWithFormat:@" 回复 %@", model.secondUserName]];
+    }
+    text = [text stringByAppendingString:[NSString stringWithFormat:@":%@", model.commentString]];
+    NSMutableAttributedString *attString = [[NSMutableAttributedString alloc] initWithString:text];
+    
+    [attString setAttributes:@{NSForegroundColorAttributeName : COLOR_333333} range:NSMakeRange(0, text.length-1)];
+    [attString setAttributes:@{NSLinkAttributeName : model.firstUserName} range:[text rangeOfString:model.firstUserName]];
+    
+    if (model.secondUserName) {
+        [attString setAttributes:@{NSLinkAttributeName : model.secondUserName} range:[text rangeOfString:model.secondUserName]];
+    }
+    return attString;
+}
+
+- (NSMutableAttributedString *)generateAttributedStringWithLikeItemModel:(SDTimeLineCellLikeItemModel *)model
+{
+    NSString *text = model.userName;
+    NSMutableAttributedString *attString = [[NSMutableAttributedString alloc] initWithString:text];
+    UIColor *highLightColor = KRedColor;
+    [attString setAttributes:@{NSForegroundColorAttributeName : highLightColor, NSLinkAttributeName : model.userId} range:[text rangeOfString:model.userName]];
+    
+    return attString;
+}
+
+
+#pragma mark - MLLinkLabelDelegate
+
+- (void)didClickLink:(MLLink *)link linkText:(NSString *)linkText linkLabel:(MLLinkLabel *)linkLabel{
+    self.didClickCommentLabelBlock(link.linkValue, CGRectMake(0, 0, KScreenWidth, 20));
+}
+- (void)didLongPressLink:(MLLink*)link linkText:(NSString*)linkText linkLabel:(MLLinkLabel*)linkLabel{
+    self.didClickLongCommentLabelBlock(link.linkValue, CGRectMake(0, 0, KScreenWidth, 20),linkLabel.tag);
+
+}
+@end
---
+++ UniversalApp/Utils/艺鑫工具/TimeLine/Views/PhotoContainer/SDWeiXinPhotoContainerView.m
@@ -0,0 +1,160 @@
+//
+//  SDWeiXinPhotoContainerView.m
+//  SDAutoLayout 测试 Demo
+//
+//  Created by gsd on 15/12/23.
+//  Copyright © 2015年 gsd. All rights reserved.
+//
+
+
+/*
+ 
+ *********************************************************************************
+ *
+ * GSD_WeiXin
+ *
+ * QQ交流群: 362419100(2群) 459274049（1群已满）
+ * Email : gsdios@126.com
+ * GitHub: https://github.com/gsdios/GSD_WeiXin
+ * 新浪微博:GSD_iOS
+ *
+ * 此“高仿微信”用到了很高效方便的自动布局库SDAutoLayout（一行代码搞定自动布局）
+ * SDAutoLayout地址：https://github.com/gsdios/SDAutoLayout
+ * SDAutoLayout视频教程：http://www.letv.com/ptv/vplay/24038772.html
+ * SDAutoLayout用法示例：https://github.com/gsdios/SDAutoLayout/blob/master/README.md
+ *
+ *********************************************************************************
+ 
+ */
+
+#import "SDWeiXinPhotoContainerView.h"
+
+#import "UIView+SDAutoLayout.h"
+
+#import "SDPhotoBrowser.h"
+
+@interface SDWeiXinPhotoContainerView () <SDPhotoBrowserDelegate>
+
+@property (nonatomic, strong) NSArray *imageViewsArray;
+
+@end
+
+@implementation SDWeiXinPhotoContainerView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        [self setup];
+    }
+    return self;
+}
+
+- (void)setup
+{
+    self.backgroundColor = KWhiteColor;
+    NSMutableArray *temp = [NSMutableArray new];
+    
+    for (int i = 0; i < 9; i++) {
+        UIImageView *imageView = [UIImageView new];
+        ViewRadius(imageView, 5);
+        [self addSubview:imageView];
+        imageView.userInteractionEnabled = YES;
+        imageView.tag = i;
+        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapImageView:)];
+        [imageView addGestureRecognizer:tap];
+        [temp addObject:imageView];
+    }
+    
+    self.imageViewsArray = [temp copy];
+}
+
+
+- (void)setPicPathStringsArray:(NSArray *)picPathStringsArray
+{
+    _picPathStringsArray = picPathStringsArray;
+    
+    for (long i = _picPathStringsArray.count; i < self.imageViewsArray.count; i++) {
+        UIImageView *imageView = [self.imageViewsArray objectAtIndex:i];
+        imageView.hidden = YES;
+    }
+    
+    if (_picPathStringsArray.count == 0) {
+        self.height = 0;
+        self.fixedHeight = @(0);
+        return;
+    }
+    
+    CGFloat itemW = [self itemWidthForPicPathArray:_picPathStringsArray];
+    CGFloat itemH = 0;
+//    if (_picPathStringsArray.count == 1) {
+//        NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:_picPathStringsArray[0]]];
+//        UIImage *image = [UIImage imageWithData:data];
+//        if (image.size.width) {
+//            itemH = image.size.height / image.size.width * itemW;
+//        }
+//    } else {
+        itemH = itemW;
+//    }
+    long perRowItemCount = [self perRowItemCountForPicPathArray:_picPathStringsArray];
+    CGFloat margin = _rowCount == 2 ? 10:5;
+    
+    [_picPathStringsArray enumerateObjectsUsingBlock:^(NSString *  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        long columnIndex = idx % perRowItemCount;
+        long rowIndex = idx / perRowItemCount;
+        UIImageView *imageView = [_imageViewsArray objectAtIndex:idx];
+        imageView.contentMode = 2;
+        imageView.hidden = NO;
+//        imageView.image = [UIImage imageNamed:obj];
+        
+        [imageView sd_setImageWithURL:[NSURL URLWithString:obj] placeholderImage:[UIImage imageNamed:@"img_moren"]];
+        imageView.frame = CGRectMake(columnIndex * (itemW + margin), rowIndex * (itemH + margin), itemW, itemH);
+    }];
+    
+    CGFloat w = perRowItemCount * itemW + (perRowItemCount - 1) * margin;
+    int columnCount = ceilf(_picPathStringsArray.count * 1.0 / perRowItemCount);
+    CGFloat h = columnCount * itemH + (columnCount - 1) * margin;
+    self.width = w;
+    self.height = h;
+    
+    self.fixedHeight = @(h);
+    self.fixedWidth = @(w);
+}
+
+#pragma mark - private actions
+
+- (void)tapImageView:(UITapGestureRecognizer *)tap
+{
+    UIView *imageView = tap.view;
+    SDPhotoBrowser *browser = [[SDPhotoBrowser alloc] init];
+    browser.currentImageIndex = imageView.tag;
+    browser.sourceImagesContainerView = self;
+    browser.imageCount = self.picPathStringsArray.count;
+    browser.delegate = self;
+    [browser show];
+}
+
+- (CGFloat)itemWidthForPicPathArray:(NSArray *)array{
+    if (self.rowCount == 1 || self.rowCount == 2) { return  self.sdWidth/2;}
+    else{return self.sdWidth/3;}
+}
+- (NSInteger)perRowItemCountForPicPathArray:(NSArray *)array{
+    return self.rowCount == 2 ? 2 : 3;
+}
+
+
+#pragma mark - SDPhotoBrowserDelegate
+
+- (NSURL *)photoBrowser:(SDPhotoBrowser *)browser highQualityImageURLForIndex:(NSInteger)index
+{
+    NSString *imageName = self.picPathStringsArray[index];
+    NSURL *url = [[NSBundle mainBundle] URLForResource:imageName withExtension:nil];
+    return url;
+}
+
+- (UIImage *)photoBrowser:(SDPhotoBrowser *)browser placeholderImageForIndex:(NSInteger)index
+{
+    UIImageView *imageView = self.subviews[index];
+    return imageView.image;
+}
+
+@end
