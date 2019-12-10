31056ea4e130e4d11ff8029a6ee648b9f60af58d
xxz888 <6952731@qq.com>
2019-12-10 17:57:03 +0800

    no message

---
+++ UniversalApp/Controller/0登录/ 完善信息/YXWanShanXinXiViewController.m
@@ -0,0 +1,32 @@
+//
+//  YXWanShanXinXiViewController.m
+//  UniversalApp
+//
+//  Created by 小小醉 on 2019/12/10.
+//  Copyright © 2019 徐阳. All rights reserved.
+//
+
+#import "YXWanShanXinXiViewController.h"
+
+@interface YXWanShanXinXiViewController ()
+
+@end
+
+@implementation YXWanShanXinXiViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
